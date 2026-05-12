898ab4ee785bf6b2c2c4da0f7b095bda685a6380
Agus <abarrosobol@frro.utn.edu.ar>
2026-05-11 21:06:48 -0300

    Primer intento de ruleta

--- TP1.1/TP1.1/main.py
+++ TP1.1/TP1.1/main.py
@@ -6,14 +6,46 @@
 # • Uso de la estructura de control FOR para iterar las listas.
 # • Empleo de funciones estadísticas.
 # • Gráficos de los resultados mediante el paquete Matplotlib.
-# • Ingreso por consola de parámetros para la simulación (cantidad de tiradas, corridas y número elegido, ejemplo
-# python programa.py -c XXX -n YYY -e ZZ).
-import mesa
+# • Ingreso por consola de parámetros para la simulación (cantidad de tiradas, corridas y número elegido)
+#   ejemplo: python programa.py -c XXX -n YYY -e ZZ.
 
+#DUDAS: clase aparte para la ruleta? Ruleta.py, algo así, en caso de que se complique
+import random
+import matplotlib.pyplot as plt
+import sys
+
+
+def ruleta_casino(cant_tiradas, cant_corridas, nro_apostado):
+    for i in range(cant_corridas):
+        random_values = [random.randint(0, 36) for _ in range(cant_tiradas)]
+
+        print("Apostaste al: ", nro_apostado)
+        print(random_values)
+        plt.figure(figsize=(10, 6))
+        plt.plot(random_values, label='Valores Aleatorios', color='blue')
+        plt.xlabel('Número de tirada')
+        plt.ylabel('Valor obtenido')
+        plt.title('Gráfico de Valores Aleatorios con Valor Constante Intermitente')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
 def main():
-    print("Simulación TP 1.1")
-    mesa.premiar()
+    try:
+        cant_corridas = int(sys.argv[2])
+        cant_tiradas = int(sys.argv[4])
+        nro_apostado = int(sys.argv[6])
+
+        if cant_tiradas >= 1 and cant_corridas >= 1 and 0 <= nro_apostado <= 36:
+            ruleta_casino(cant_tiradas, cant_corridas, nro_apostado)
+        else:
+            print("Argumentos inválidos")
+            return
+
+    except ValueError,IndexError:
+        print("No se puede ejecutar el programa. Uso correcto: python main.py -c XXX -n YYY -e ZZ")
+        return
+
 
 
 # Esta es una buena práctica en Python 3 para indicar que main() solo debe ejecutarse si ejecutamos ESTE archivo directamente.
--- TP1.1/TP1.1/mesa.py
+++
@@ -1,6 +0,0 @@
-#Métodos que asignan premios según el resultado de la tirada
-#También estaría bueno que sea una clase con atributos
-import ruleta
-
-def premiar():
-    print(f"El resultado de la tirada fue: {ruleta.tirada()}")
